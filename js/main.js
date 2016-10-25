ceac819ca64fa4075ae22f55c1e1d64aeb0993fb
David Tan <davified@gmail.com>
2016-10-25 11:16:18 +0800

    added front page

--- js/main.js
+++ js/main.js
@@ -1,62 +1,56 @@
 /* globals $ */
 // check out document ready turbolinks
 $(document).ready(function () {
+  var granimInstance = new Granim({
+    element: '#canvas-interactive',
+    name: 'interactive-gradient',
+    elToSetClassOn: '.canvas-interactive-wrapper',
+    direction: 'diagonal',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    stateTransitionSpeed: 800,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#B3FFAB', '#12FFF7'],
+                ['#1A2980', '#26D0CE']
+            ],
+            transitionSpeed: 5000
+        },
+        "violet-state": {
+            gradients: [
+                ['#9D50BB', '#6E48AA'],
+                ['#4776E6', '#8E54E9']
+            ],
+            transitionSpeed: 2000
+        },
+        "orange-state": {
+            gradients: [ ['#FF4E50', '#F9D423'] ],
+            loop: false
+        }
+    }
+});
 
-  
-  // var granimInstance = new Granim({
-  //   element: '#canvas-interactive',
-  //   name: 'interactive-gradient',
-  //   elToSetClassOn: '.canvas-interactive-wrapper',
-  //   direction: 'diagonal',
-  //   opacity: [1, 1],
-  //   isPausedWhenNotInView: true,
-  //   stateTransitionSpeed: 500,
-  //   states: {
-  //     'default-state': {
-  //       gradients: [
-  //         ['#B3FFAB', '#12FFF7'],
-  //         ['#1A2980', '#26D0CE'],
-  //         ['#13bcc9', '#93fb9a']
-  //
-  //       ],
-  //       transitionSpeed: 3500
-  //     },
-  //     'violet-state': {
-  //       gradients: [
-  //         ['#9D50BB', '#6E48AA'],
-  //         ['#4776E6', '#2645c6']
-  //       ],
-  //       transitionSpeed: 2000
-  //     },
-  //     'orange-state': {
-  //       gradients: [ ['#0853ee', '#35a486'],
-  //                     ['#4776E6', '#2645c6'],
-  //                     ['#3da9d9','#0f1457']
-  //       ],
-  //       transitionSpeed: 2500
-  //     }
-  //   }
-  // })
-  //
-  // // With jQuery
-  // $('#default-state-cta').on('mouseover', function (event) {
-  //   event.preventDefault()
-  //   granimInstance.changeState('default-state')
-  //   setClass('#default-state-cta')
-  // })
-  // $('#violet-state-cta').on('mouseover', function (event) {
-  //   event.preventDefault()
-  //   granimInstance.changeState('violet-state')
-  //   setClass('#violet-state-cta')
-  // })
-  // $('#orange-state-cta').on('mouseover', function (event) {
-  //   event.preventDefault()
-  //   granimInstance.changeState('orange-state')
-  //   setClass('#orange-state-cta')
-  // })
-  //
-  // function setClass (element) {
-  //   $('.canvas-interactive-wrapper a').removeClass('active')
-  //   $(element).addClass('active')
-  // }
+// With jQuery
+$('#default-state-cta').on('mouseover', function(event) {
+    event.preventDefault();
+    granimInstance.changeState('default-state');
+    setClass('#default-state-cta')
+});
+$('#violet-state-cta').on('mouseover', function(event) {
+    event.preventDefault();
+    granimInstance.changeState('violet-state');
+    setClass('#violet-state-cta')
+});
+$('#orange-state-cta').on('mouseover', function(event) {
+    event.preventDefault();
+    granimInstance.changeState('orange-state');
+    setClass('#orange-state-cta')
+});
+
+function setClass(element) {
+    $('.canvas-interactive-wrapper a').removeClass('active');
+    $(element).addClass('active');
+};
+
 })
